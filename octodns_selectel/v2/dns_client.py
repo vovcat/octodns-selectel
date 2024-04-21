6812f1cac251f9b9d72734407eb0aad37d965dda
Volodymyr Prodan <vovcat@gmail.com>
2024-04-21 21:38:41 +0300

    Cleanup, add DNAME support, request zone list only if necessary, etc.
    Connect to the server on first request instead of initialization.
    Use decoded_name instead of idna_decode.
    Consistently use zone instead of zone_name.
    Raise _PAGINATION_LIMIT from 50 to 1000.
    Retry request in case of timeout.
    Move imports to top of the file.
    Add more logging.

--- octodns_selectel/v2/dns_client.py
+++ octodns_selectel/v2/dns_client.py
@@ -1,5 +1,6 @@
+from logging import getLogger
 from requests import Session
-
+from requests.exceptions import ReadTimeout
 from octodns import __version__ as octodns_version
 
 from .exceptions import ApiException
@@ -7,21 +8,22 @@ from .exceptions import ApiException
 
 class DNSClient:
     API_URL = 'https://api.selectel.ru/domains/v2'
-    _PAGINATION_LIMIT = 50
+    _PAGINATION_LIMIT = 1000
 
     _zone_path = "/zones"
     __rrsets_path = "/zones/{}/rrset"
     __rrsets_path_specific = "/zones/{}/rrset/{}"
 
     def __init__(self, library_version: str, openstack_token: str):
-        self._sess = Session()
-        self._sess.headers.update(
-            {
-                'X-Auth-Token': openstack_token,
-                'Content-Type': 'application/json',
-                'User-Agent': f'octodns/{octodns_version} octodns-selectel/{library_version}',
-            }
-        )
+        from logging import getLogger
+        self.log = getLogger(self.__class__.__name__)
+        self.log.debug(f'__init__: library_version=%s', library_version)
+        self._sess_headers = {
+            'X-Auth-Token': openstack_token,
+            'Content-Type': 'application/json',
+            'User-Agent': f'octodns/{octodns_version} octodns-selectel/{library_version}',
+        }
+        self._sess = None
 
     @classmethod
     def _rrset_path(cls, zone_id):
@@ -32,12 +34,24 @@ class DNSClient:
         return cls.__rrsets_path_specific.format(zone_id, rrset_id)
 
     def _request(self, method, path, params=None, data=None):
+        if not self._sess:
+            self._sess = Session()
+            self._sess.headers.update(self._sess_headers)
+
         url = f'{self.API_URL}{path}'
-        resp = self._sess.request(method, url, params, json=data)
+        self.log.info(f'_request: {method}, {url}, {params}')
+        self.log.debug(f'_request: ... {data}')
+
+        resp = None
+        while resp is None:
+            try: resp = self._sess.request(method, url, params, json=data, timeout=(8,10))
+            except ReadTimeout as e: self.log.warning(f'timeout, retrying')
+
         try:
             resp_json = resp.json()
         except ValueError:
             resp_json = {}
+
         if resp.status_code in {200, 201, 204}:
             return resp_json
         elif resp.status_code in {400, 422}:
@@ -45,11 +59,12 @@ class DNSClient:
                 f'Bad request. Description: {resp_json.get("description", "Invalid payload")}.'
             )
         elif resp.status_code == 401:
-            raise ApiException('Authorization failed. Invalid or empty token.')
+            raise ApiException(
+                f'Authorization failed. Invalid or empty token.'
+            )
         elif resp.status_code == 404:
             raise ApiException(
-                'Resource not found: '
-                f'{resp_json.get("error", "invalid path")}.'
+                f'Resource not found: {resp_json.get("error", "invalid path")}.'
             )
         elif resp.status_code == 409:
             raise ApiException(
@@ -58,18 +73,18 @@ class DNSClient:
         else:
             raise ApiException('Internal server error.')
 
-    def _request_all_entities(self, path, offset=0):
+    def _request_all_entities(self, path, offset=0, **kwa):
         items = []
         resp = self._request(
-            "GET", path, dict(limit=self._PAGINATION_LIMIT, offset=offset)
+            "GET", path, dict(limit=self._PAGINATION_LIMIT, offset=offset, **kwa)
         )
         items.extend(resp["result"])
         if next_offset := resp["next_offset"]:
             items.extend(self._request_all_entities(path, offset=next_offset))
         return items
 
-    def list_zones(self):
-        return self._request_all_entities(self._zone_path)
+    def list_zones(self, filter=''):
+        return self._request_all_entities(self._zone_path, filter=filter)
 
     def create_zone(self, name):
         return self._request('POST', self._zone_path, data=dict(name=name))
